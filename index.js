54bb152f17f89d7eca2fee3b36bc0c9f28464131
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-28 14:07:13 +0700

    let azure describes an image

--- index.js
+++ index.js
@@ -13,5 +13,12 @@ const computerVisionClient = new ComputerVisionClient(
     new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
 
 (async () => {
+
+    let imageURL = 'https://www.thesprucepets.com/thmb/beAAL4NaD8_zLL0BM13Tv6IbqV0=/1500x1000/filters:fill(auto,1)/kitten-56a09ff83df78cafdaa36304.jpg';
+
+    console.log('Analyzing URL image to describe...', imageURL.split('/').pop());
+    const caption = (await computerVisionClient.describeImage(imageURL)).captions[0];
+    console.log(`This may be ${caption.text} (${caption.confidence.toFixed(2)} confidence)`);
+
     console.log('-----');
 })()
