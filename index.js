2d49bfdd039f82166a77ededbe98f6a83548fd8c
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-28 14:00:26 +0700

    authenticate node app with service

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 require('dotenv').config();
 const key = process.env.key;
 const endpoint = process.env.endpoint;
+
+const fs = require('fs');
+const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
+const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+
+
+
+const computerVisionClient = new ComputerVisionClient(
+    new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
+
+(async () => {
+    console.log('-----');
+})()
